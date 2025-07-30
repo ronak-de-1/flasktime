ed7c6f4c7147ba276e87284c02af5cbc7b180442
Ronak <ronak.de@gmail.com>
2025-07-30 15:58:03 +0100

    template

---
+++ static/js/login.js
@@ -0,0 +1,30 @@
+$('#loginForm').on('submit', function(e) {
+    e.preventDefault();
+
+    var username = $('#username').val();
+    var password = $('#password').val();
+
+    // Create a JS expression string for $where injection
+    var wherePayload = `this.username == '${username}' && this.password == '${password}'`;
+
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            $where: wherePayload  // Send raw JS expression for backend $where query
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.result === 'success') {
+            window.location.href = '/prizes';
+        } else {
+            alert(data.result);
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+});
